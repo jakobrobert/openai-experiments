fd08cd4d3037caebfd4ded6f180a524e2a3240e2
jakobrobert <jakob-robert@hotmail.de>
2024-09-01 20:46:42 +0200

    Add .gitignore
    move main.py, no separate directory for console-app needed because it will be removed soon anyway

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ client = OpenAI(
 )
 
 
+# TODO Create method to generate motivational quotes. Add params language, tone, verbosity.
+#  Maybe also how well performed in recent days. For the params, add a number to indicate intensity.
 def send_prompt(prompt):
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
@@ -19,6 +21,8 @@ def send_prompt(prompt):
 if __name__ == "__main__":
     while True:
         user_input = input("You: ")
+
+        # TODO adjust input handling. user should input the different parameters, then show the motivational quote.
         if user_input.lower() in ["exit"]:
             break
 
