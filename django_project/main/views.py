f6ef94c9599de6ad11b0a833b45242a886cba9ff
jakobrobert <jakob-robert@hotmail.de>
2024-09-20 20:59:10 +0200

    Remove fallback for generate_quote if method is not POST

--- django_project/main/views.py
+++ django_project/main/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_POST
 
 
 def index(request):
     return render(request, 'index.html')
 
 
+@require_POST
 def generate_quote(request):
-    if request.method == 'POST':
-        language = request.POST.get('language')
-        tone = request.POST.get('tone')
-        verbosity = request.POST.get('verbosity')
+    language = request.POST.get('language')
+    tone = request.POST.get('tone')
+    verbosity = request.POST.get('verbosity')
 
-        # TODO use OpenAI API to generate quote
-        quote = f'Generate quote with following params: language: {language}, tone: {tone}, verbosity: {verbosity}'
+    # TODO use OpenAI API to generate quote
+    quote = f'Generate quote with following params: language: {language}, tone: {tone}, verbosity: {verbosity}'
 
-        return render(request, 'index.html', {'quote': quote})
+    return render(request, 'index.html', {'quote': quote})
