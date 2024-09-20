01b8e9590e6fef5615c3fcf1a8983916e36ea874
jakobrobert <jakob-robert@hotmail.de>
2024-09-20 20:42:44 +0200

    Add form to generate quote (no functionality yet)

--- django_project/main/views.py
+++ django_project/main/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def generate_quote(request):
+    if request.method == 'POST':
+        language = request.POST.get('language')
+        tone = request.POST.get('tone')
+        verbosity = request.POST.get('verbosity')
+
+        # TODO use OpenAI API to generate quote
+        quote = f'Generate quote with following params: language: {language}, tone: {tone}, verbosity: {verbosity}'
+
+        return render(request, 'index.html', {'quote': quote})
