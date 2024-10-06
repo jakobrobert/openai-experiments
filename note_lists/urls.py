945e150a287a9528b2566b86b1c59cc5a082dbd1
jakobrobert <jakob-robert@hotmail.de>
2024-10-06 17:15:11 +0200

    Add content to note_lists.html

--- note_lists/urls.py
+++ note_lists/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.note_lists, name='note_lists'),
+    path('add/', views.add_note_list, name="add_note_list")
 ]
--- note_lists/views.py
+++ note_lists/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def note_lists(request):
     return render(request, 'note_lists.html')
+
+
+def add_note_list(request):
+    pass
