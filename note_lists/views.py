0cd5d1c66357123fec4fdaa449782c253c365109
jakobrobert <jakob-robert@hotmail.de>
2024-10-12 16:01:12 +0200

    Render note lists in template

--- note_lists/views.py
+++ note_lists/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
+from .models import NoteList
+
 
 def note_lists(request):
-    return render(request, 'note_lists.html')
+    note_lists = NoteList.objects.all()
+    return render(request, 'note_lists.html', {'note_lists': note_lists})
 
 
 def add_note_list(request):
