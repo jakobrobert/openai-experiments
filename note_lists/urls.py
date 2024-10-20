d5bf4c5d92c98b33b0c31ebee94a19964ba6d7db
jakobrobert <jakob-robert@hotmail.de>
2024-10-20 14:10:13 +0200

    Adjust url for add_note

--- note_lists/urls.py
+++ note_lists/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('add/', views.add_note_list, name='add_note_list'),
     path('<int:note_list_id>/', views.get_note_list, name='get_note_list'),
     path('<int:note_list_id>/delete/', views.delete_note_list, name='delete_note_list'),
-    path('<int:note_list_id>/add/', views.add_note, name='add_note'),
+    path('<int:note_list_id>/notes/add/', views.add_note, name='add_note'),
     path('<int:note_list_id>/notes/<int:note_id>/delete/', views.delete_note, name='delete_note'),
 ]
