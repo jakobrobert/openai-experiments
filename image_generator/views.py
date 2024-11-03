1bf7255c0463337bc69a0556f830cacded38fde1
jakobrobert <jakob-robert@hotmail.de>
2024-11-03 17:47:03 +0100

    Implement logic for generate_image.
    Not fully integrated yet into view, but basically works fine.

--- image_generator/views.py
+++ image_generator/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_POST
 
+from utils import generate_openai_image
+
 
 def image_generator(request):
     # TODO Get params & send to template by context
@@ -8,6 +10,12 @@ def image_generator(request):
 
 
 @require_POST
-def generate_image():
-    # TODO Implement image generation
+def generate_image(request):
+    prompt = request.POST.get('prompt')
+
+    image = generate_openai_image(prompt)
+
+    print(f'revised_prompt: {image.revised_prompt}')
+    print(f'url: {image.url}')
+
     return None
